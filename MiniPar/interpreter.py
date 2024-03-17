49c68e84b3706f8ab4d7d976faf5b8832ea2ff55
jaofe <jaof@ic.ufal.br>
2024-03-16 21:04:17 -0300

    input e print funcionando

--- MiniPar/interpreter.py
+++ MiniPar/interpreter.py
@@ -21,7 +21,7 @@ class SymbolTable:
     def set_value(self, name, value):
         self.symbols[name] = value
 
-def arithExpr(expr, symbol_table):
+def parseExpr(expr, symbol_table):
     if len(expr) == 0:
         #TODO throw error
         return "Syntax Error"
@@ -31,16 +31,13 @@ def arithExpr(expr, symbol_table):
     for token in expr:
         if token.type == 'IDENTIFIER':
             expr_str += str(symbol_table.get_value(token.value))
-        elif token.type == 'INT' or 'PLUS' or 'MINUS' or 'MULTIPLY' or 'DIVIDE' or 'L_PAREN' or 'R_PAREN':
+        elif token.type == 'INT' or 'BOOL' or 'PLUS' or 'MINUS' or 'MULTIPLY' or 'DIVIDE' or 'GREATER_EQUAL' or 'EQUAL_EQUAL' or 'EQUAL_EQUAL' or 'NOT_EQUAL' or 'R_PAREN' or 'L_PAREN' or 'R_PAREN':
             expr_str += str(token.value)
         else:
             #TODO throw error
             return "Syntax Error"
         
     return eval(expr_str)
-
-def boolExpr(expr):
-    pass
 
 def intParser(tokens, symbol_table):
     if tokens[0].type != 'IDENTIFIER':
@@ -50,16 +47,17 @@ def intParser(tokens, symbol_table):
     identifier_name = tokens[0].value
     symbol_table.add_symbol(identifier_name)
     
-    if tokens[1].type == 'EQUALS':
+    if tokens[1].type == 'EQUALS':        
         expr = []
         i = 2
-        while (tokens[i].type != 'SEMICOLON'):
-            expr.append(tokens[i])
-            i += 1
-        
-        symbol_table.set_value(identifier_name, arithExpr(expr, symbol_table))
-        
-        print(symbol_table.get_value(identifier_name))
+        if tokens[2].type == 'INPUT':
+            symbol_table.set_value(identifier_name, input())
+        else:
+            while (tokens[i].type != 'SEMICOLON'):             
+                expr.append(tokens[i])
+                i += 1
+
+            symbol_table.set_value(identifier_name, parseExpr(expr, symbol_table))
         
     elif tokens[1].type == 'SEMICOLON':
         return
@@ -78,8 +76,14 @@ def BOOL(tokens):
 def IF(condition, tokens):
     pass
 
-def WHILE(condition, tokens):
-    pass
+def whileParser(conditions, tokens, symbol_table):
+    print("conditions")
+    for condition in conditions:
+        print(condition.value)
+    
+    print("tokens")
+    for token in tokens:
+        print(token.value)
         
 def block_stmts(block_tokens):        
     interpreter = Interpreter(block_tokens)
@@ -94,9 +98,7 @@ class Interpreter:
         
         i = 0;
         while True:
-            
             token = self.tokens[i]
-            print(token.type)
             
             i += 1
             if token.type == 'SEQ':                 
@@ -165,10 +167,81 @@ class Interpreter:
                 pass         
             elif token.type == 'STRING':
                 pass
+            elif token.type == 'PRINT':
+                if self.tokens[i].type != 'L_PAREN':
+                    return('Syntax Error')
+                i += 1
+                paren_count = 1
+                expr_tokens = []
+                while(True):
+                    if self.tokens[i].type == 'L_PAREN':
+                        paren_count += 1
+                    elif self.tokens[i].type == 'R_PAREN':
+                        paren_count -= 1
+                    elif(self.tokens[i].type == 'EOF'):   
+                        #TODO throw error
+                        return('Syntax Error')
+                    if(paren_count == 0):
+                        i += 1
+                        break
+                    
+                    expr_tokens.append(self.tokens[i])
+                    
+                    i += 1;
+                i += 1;  
+                    
+                if (len(expr_tokens) == 1) and (expr_tokens[0].type != 'IDENTIFIER'):
+                    print(expr_tokens[0].value)
+                else:
+                    print(parseExpr(expr_tokens, self.symbol_table))               
+                    
             elif token.type == 'IF':
                 pass
             elif token.type == 'WHILE':
-                pass
+                if self.tokens[i].type != 'L_PAREN':
+                    return('Syntax Error')
+                i += 1
+                paren_count = 1
+                condition_tokens = []
+                while(True):
+                    if self.tokens[i].type == 'L_PAREN':
+                        paren_count += 1
+                    elif self.tokens[i].type == 'R_PAREN':
+                        paren_count -= 1
+                    elif(self.tokens[i].type == 'EOF'):   
+                        #TODO throw error
+                        return('Syntax Error')
+                    if(paren_count == 0):
+                        i += 1
+                        break
+                    
+                    condition_tokens.append(self.tokens[i])
+                    
+                    i += 1;
+                    
+                if self.tokens[i].type != 'L_BRACE':
+                    return('Syntax Error')
+                i += 1
+                brace_count = 1
+                block_tokens = []
+                while(True):
+                    if self.tokens[i].type == 'L_BRACE':
+                        brace_count += 1
+                    elif self.tokens[i].type == 'R_BRACE':
+                        brace_count -= 1
+                    elif(self.tokens[i].type == 'EOF'):   
+                        #TODO throw error
+                        return('Syntax Error')
+                    if(brace_count == 0):
+                        i += 1
+                        break
+                    block_tokens.append(self.tokens[i])
+                    i += 1
+
+                block_tokens.append(Token('EOF', 'EOF'))
+                
+                whileParser(condition_tokens, block_tokens, self.symbol_table)
+                
             elif token.type == 'EOF':
                 break;
             else:
